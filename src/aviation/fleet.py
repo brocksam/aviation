7d493c7a0a1795ef06f2696de30aac5623784c38
Sam Brockie <sgb39@cam.ac.uk>
2025-07-22 17:23:04 +0100

    Add `__all__`

--- src/aviation/fleet.py
+++ src/aviation/fleet.py
@@ -1,5 +1,7 @@
 """Modelling of the global fleet based on average passenger and aircraft data."""
 
+__all__ = ("passengers_per_day", "required_global_fleet")
+
 from aviation._model import transform
 
 
