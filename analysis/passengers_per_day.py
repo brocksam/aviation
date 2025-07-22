19320ce3bb3003cbebba157e1d7f42c873d50e1b
Sam Brockie <sgb39@cam.ac.uk>
2025-07-22 17:18:33 +0100

    Remove unused input variables

--- analysis/passengers_per_day.py
+++ analysis/passengers_per_day.py
@@ -4,8 +4,6 @@ import aviation
 
 days_per_year = 365.0
 passengers_per_year = 5_000_000_000.0
-seats_per_aircraft = 150.0
-flights_per_aircraft_per_day = 2.0
 
 passengers_per_day = aviation.passengers_per_day(passengers_per_year, days_per_year)
 print(f"{passengers_per_day=}")
