acbaed760568d6bc5621c7d0eddee9008de12842
Sam Brockie <sgb39@cam.ac.uk>
2025-07-23 11:07:02 +0100

    Refactor analysis scripts to use `SystemsModel.evaluate`

--- analysis/passengers_per_day.py
+++ analysis/passengers_per_day.py
@@ -1,9 +1,14 @@
 """Analysis to determine the number of passengers per day globally."""
 
 import aviation
+from aviation import _engine as engine
 
-days_per_year = 365.0
+days_per_year = 365.25
 passengers_per_year = 5_000_000_000.0
 
-passengers_per_day = aviation.passengers_per_day(passengers_per_year, days_per_year)
+inputs = {"days_per_year": days_per_year, "passengers_per_year": passengers_per_year}
+output = "passengers_per_day"
+
+systems_model = engine.SystemsModel(aviation.transforms)
+passengers_per_day = systems_model.evaluate(inputs, output)
 print(f"{passengers_per_day=}")
