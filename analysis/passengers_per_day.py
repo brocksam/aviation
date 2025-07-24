895075cd4dbb6910d5690e906df75c19643e16dc
Sam Brockie <sgb39@cam.ac.uk>
2025-07-24 10:39:30 +0100

    Refactor repository to use camia-model's units

--- analysis/passengers_per_day.py
+++ analysis/passengers_per_day.py
@@ -1,15 +1,16 @@
 """Analysis to determine the number of passengers per day globally."""
 
 import camia_engine as engine
+from camia_model.units import year
 
 import aviation
+from aviation.units import passenger
 
-days_per_year = 365.25
-passengers_per_year = 5_000_000_000.0
+passengers_per_year = 5_000_000_000.0 * passenger / year
 
-inputs = {"days_per_year": days_per_year, "passengers_per_year": passengers_per_year}
+inputs = {"passengers_per_year": passengers_per_year}
 output = "passengers_per_day"
 
 systems_model = engine.SystemsModel(aviation.transforms)
 passengers_per_day = systems_model.evaluate(inputs, output)
-print(f"{passengers_per_day=}")
+print(f"{passengers_per_day=!s}")
